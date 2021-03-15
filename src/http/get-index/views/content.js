96568baf30394fef9159148c111b571e48cfddcb
Juan <jmventura@mac.com>
2021-03-15 14:51:09 +0100

    Update content.js

--- src/http/get-index/views/content.js
+++ src/http/get-index/views/content.js
@@ -6,7 +6,7 @@ import SocialMedia from './social-media.js'
 import Icon from './icon.js'
 
 export default function Content(props = {}) {
-  let fullname = props.fullname || ''
+  let fullname = props.fullname || 'Juan Manuel Ventura'
   let occupation = props.occupation || ''
   let location = props.location || ''
   let bio = props.bio || ''
